a9b7582651eeaa1ee693cab93ca0870305a63d53
k6dy <egocody@gmail.com>
2025-04-08 20:24:25 -0700

    L43 (2) CJ F - 1st

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
     total = 0
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
+    i = 0
+    while i < 5:
+        num = int(input("Enter a number: "))
+        total += num
+        i += 1
+
     return total
 
 
 if __name__ == '__main__':
-    main()
+    result = main()
+    print(result)
